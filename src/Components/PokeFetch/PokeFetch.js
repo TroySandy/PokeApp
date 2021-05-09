8b7312ad502da06eeeabd65f43fa64813be56f31
TroySandy <troysandy16@gmail.com>
2021-05-09 16:12:33 -0400

    working

--- src/Components/PokeFetch/PokeFetch.js
+++ src/Components/PokeFetch/PokeFetch.js
@@ -1,46 +1,121 @@
-import React, { Component } from 'react'
-import './PokeFetch.css';
-
+import React, { Component } from "react";
+import "./PokeFetch.css";
+// import CountDownTimer from '../Timer/Timer'
 
 class PokeFetch extends Component {
   constructor() {
-    super()
+    super();
     this.state = {
-      pokeInfo: '',
-      pokeSprite: '',
-      pokeName: '',
-    }
+      pokeInfo: "",
+      pokeSprite: "",
+      pokeName: "",
+    };
   }
 
-  fetchPokemon() {
+  fetchPokemon = () => {
     let min = Math.ceil(1);
-    let max = Math.floor(152);
+    let max = Math.floor(500);
     let pokeNum = Math.floor(Math.random() * (max - min) + min);
     fetch(`https://pokeapi.co/api/v2/pokemon/${pokeNum}`, {
-      method: 'GET'
-    }).then(res => res.json())
-      .then(res => {
+      method: "GET",
+    })
+      .then((res) => res.json())
+      .then((res) => {
         this.setState({
           pokeInfo: res,
           pokeSprite: res.sprites.front_default,
           pokeName: res.species.name,
-        })
+        });
       })
-      .catch((err) => console.log(err))
-  }
+      .catch((err) => console.log(err));
+  };
 
   render() {
     return (
-      <div className={'wrapper'}>
-        <button className={'start'} onClick={() => this.fetchPokemon()}>Start!</button>
-        <h1 className={'timer'} >Timer Display</h1>
-        <div className={'pokeWrap'}>
-          <img className={'pokeImg'} src={this.state.pokeSprite} />
-          <h1 className={'pokeName'}>{this.state.pokeName}</h1>
+      <div className={"wrapper"}>
+        <Timer pokeFetch={this.fetchPokemon} img={this.state.pokeSprite} name={this.state.pokeName}/>
+        <div className={"pokeWrap"}>
         </div>
       </div>
-    )
+    );
   }
 }
 
 export default PokeFetch;
+
+class Timer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      seconds: 10,
+      answers: 0,
+    };
+  }
+
+  startTimer = () => {
+    this.timer = setInterval(this.countDown, 1000);
+    this.props.pokeFetch();
+  };
+
+  countDown = () => {
+    const { seconds } = this.state;
+    let c_seconds = seconds;
+
+    if (c_seconds) {
+      // seconds change
+      seconds
+        ? this.setState({ seconds: seconds - 1 })
+        : this.setState({ seconds: 10 });
+    } else {
+      clearInterval(this.timer);
+    }
+  };
+
+  correct = () => {
+    this.setState({
+      seconds: 10,
+      answers: this.state.answers + 1,
+    });
+  };
+
+  wrong = () => {
+    this.setState({
+      seconds: 10,
+      answers: this.state.answers - 1,
+    });
+  };
+
+  render() {
+    const { seconds, answers } = this.state;
+
+    return (
+      <div className="timer">
+        <div >
+          <button onClick={this.startTimer} className="start">
+            Start
+          </button>
+        <h1> You have {seconds} seconds <br/><br/> left to guess!! </h1>
+        {(!this.props.img) ? <> </> : 
+        <>
+        <img className={"pokeImg"} src={this.props.img} />
+        <h1 className={"pokeName"}>{this.props.name}</h1>
+        </>
+        }
+        <div>
+        <h3> You currently have guessed {answers} correctly</h3>
+          <div className='after'>
+          <h1>Did you get it </h1>
+          <button onClick={this.correct} className="answers">
+            Right!!!
+          </button>
+          <span> OR </span>
+          <button onClick={this.wrong} className="answers">
+            Wrong....
+          </button>
+        </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
